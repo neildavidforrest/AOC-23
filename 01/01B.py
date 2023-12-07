3c9f3627e87fe52d60c13e32e4fdbaa54cf425b5
Neil Forrest <neil.forrest@sensegraphics.com>
2023-12-07 17:01:01 +0000

    Simplify day 1

--- 01/01B.py
+++ 01/01B.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from typing import Optional
 
 names = {
     "one": 1,
@@ -13,50 +14,34 @@ names = {
 }
 
 
-def to_digits(input: str):
-    min_idx = int(float("inf"))
-    min_value = ""
-    min_key = ""
-    max_idx = int(float("-inf"))
-    max_value = ""
-    max_key = ""
-    for key, value in names.items():
-        idx = input.find(key)
-        if idx != -1 and idx < min_idx:
-            min_idx = idx
-            min_value = value
-            min_key = key
-        idx = input.rfind(key)
-        if idx != -1 and idx > max_idx:
-            max_idx = idx
-            max_value = value
-            max_key = key
-
-    overlap = 0
-    if max_idx >= 0 and max_idx != min_idx:
-        if max_idx < min_idx + len(min_key):
-            overlap = min_idx + len(min_key) - max_idx
-        input = input[0:max_idx] + str(max_value) + input[max_idx + len(max_key) :]
-    if min_idx >= 0:
-        input = (
-            input[0:min_idx]
-            + str(min_value)
-            + input[min_idx + len(min_key) - overlap :]
-        )
-    return input
+def to_digit(input: str, index: int) -> Optional[int]:
+    if input[index].isdigit():
+        return int(input[index])
+    else:
+        for name, number in names.items():
+            if input[index:index+len(name)] == name:
+                return number
+    return None
 
 
-def compute_value(input: str):
-    return int(
-        [x for x in input if x.isdigit()][0]
-        + [x for x in reversed(input) if x.isdigit()][0]
-    )
-
+def compute_value(input: str) -> int:
+    first = None
+    last = None
+    for i in range(len(input)):
+        if first is None:
+            d = to_digit(input, i)
+            if d is not None:
+                first = d
+        if last is None:
+            d = to_digit(input, len(input) - 1 - i)
+            if d is not None:
+                last = d
+    return int(f"{first}{last}")
 
 sum = 0
 with open("input.txt") as f:
     line = f.readline()
     while line:
-        sum += compute_value(to_digits(line))
+        sum += compute_value(line)
         line = f.readline()
 print(sum)
